5835ae4445d72e1caf7333cab5292c0b85a571fc
lnc4288 <lnc4288@outlook.com>
2017-07-08 12:51:04 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FaqComponent } from './faq/faq.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: HomeComponent
+  },
+  {
+    path: 'faq',
+    component: FaqComponent
   }
 ];
 
